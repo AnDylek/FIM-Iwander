19a70fe0c4c5ff7a7d631aa3d071bee04b612031
AnDylek <AnDylek@DESKTOP-OB348PM>
2016-05-11 14:15:53 +0200

    Initialize

---
+++ Iwander/src/iwander/Model/VyhledaniLetu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iwander.Model;
+
+import iwander.Database.Connect;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author AnDylek
+ */
+public class VyhledaniLetu extends Connect{
+
+    public VyhledaniLetu(Connection conn, Statement stmt) {
+        super(conn, stmt);
+    }
+    public void vyhledej(String odkud, String kam, SimpleDateFormat sdf){
+        
+    }
+
+}
